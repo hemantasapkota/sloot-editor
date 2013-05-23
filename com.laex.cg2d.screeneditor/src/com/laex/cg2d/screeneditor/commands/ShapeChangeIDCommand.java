adf288f82ddcd27cde377b3dd24a9d160b086bc2
Hemanta Sapkota <laex.pearl@gmail.com>
2013-05-23 17:27:05 +1000

    Command for changing the shape id. Using command approach is good, as it allows for undo/redo of actions.

---
+++ com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/commands/ShapeChangeIDCommand.java
@@ -0,0 +1,35 @@
+package com.laex.cg2d.screeneditor.commands;
+
+import org.eclipse.gef.commands.Command;
+
+import com.laex.cg2d.shared.model.Shape;
+
+public class ShapeChangeIDCommand extends Command {
+  
+  private Shape shape;
+  private String newId;
+  private String oldId;
+
+  public ShapeChangeIDCommand(Shape shape, String newId) {
+    this.shape = shape;
+    this.newId = newId;
+    this.oldId = shape.getId();
+    setLabel("Change ID");
+  }
+  
+  @Override
+  public void execute() {
+    redo();
+  }
+  
+  @Override
+  public void redo() {
+    shape.setId(newId);
+  }
+  
+  @Override
+  public void undo() {
+    shape.setId(oldId);
+  }
+
+}
