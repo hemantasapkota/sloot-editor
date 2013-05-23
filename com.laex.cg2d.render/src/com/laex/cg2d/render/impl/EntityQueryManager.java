6774cbdd61bfad803bb896e1ab6ff7e6c192d8c1
Hemanta Sapkota <laex.pearl@gmail.com>
2013-05-23 17:19:35 +1000

    Minor refactor.

--- com.laex.cg2d.render/src/com/laex/cg2d/render/impl/EntityQueryManager.java
+++ com.laex.cg2d.render/src/com/laex/cg2d/render/impl/EntityQueryManager.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2012, 2013 Hemanta Sapkota.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Hemanta Sapkota (laex.pearl@gmail.com)
+ */
 package com.laex.cg2d.render.impl;
 
 import java.util.Iterator;
@@ -7,14 +17,26 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.laex.cg2d.protobuf.ScreenModel.CGShape;
 import com.laex.cg2d.render.EntityQueryable;
 
+/**
+ * The Class EntityQueryManager.
+ */
 public class EntityQueryManager implements EntityQueryable {
 
+  /** The world. */
   private World world;
 
+  /**
+   * Instantiates a new entity query manager.
+   *
+   * @param world the world
+   */
   public EntityQueryManager(World world) {
     this.world = world;
   }
 
+  /* (non-Javadoc)
+   * @see com.laex.cg2d.render.EntityQueryable#getEntityById(java.lang.String)
+   */
   @Override
   public Body getEntityById(String id) {
     Iterator<Body> body = world.getBodies();
@@ -32,6 +54,9 @@ public class EntityQueryManager implements EntityQueryable {
     return null;
   }
 
+  /* (non-Javadoc)
+   * @see com.laex.cg2d.render.EntityQueryable#getEntityIdByBody(com.badlogic.gdx.physics.box2d.Body)
+   */
   @Override
   public String getEntityIdByBody(Body b) {
     return ((CGShape) b.getUserData()).getId();
