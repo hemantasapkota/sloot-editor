cdb9e16b4db74d2274ba8bc724b33e47fe202444
Hemanta Sapkota <laex.pearl@gmail.com>
2013-05-23 17:17:06 +1000

    Added code comments.

--- com.laex.cg2d.render/src/com/laex/cg2d/render/BodyVisitor.java
+++ com.laex.cg2d.render/src/com/laex/cg2d/render/BodyVisitor.java
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) 2012, 2013 Hemanta Sapkota.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Hemanta Sapkota (laex.pearl@gmail.com)
+ */
 package com.laex.cg2d.render;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.laex.cg2d.protobuf.ScreenModel.CGShape;
 
+/**
+ * The Interface BodyVisitor.
+ */
 public interface BodyVisitor {
   
+  /**
+   * Visit.
+   *
+   * @param b the b
+   * @param shape the shape
+   */
   void visit(Body b, CGShape shape);
 
 }
