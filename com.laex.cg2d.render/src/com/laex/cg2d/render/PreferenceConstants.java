9c2a90ea00d981c6ac2fd11ae76e005356e76c4b
Hemanta Sapkota <laex.pearl@gmail.com>
2013-05-23 17:17:56 +1000

    Comment out unused Preferences constants.

--- com.laex.cg2d.render/src/com/laex/cg2d/render/PreferenceConstants.java
+++ com.laex.cg2d.render/src/com/laex/cg2d/render/PreferenceConstants.java
@@ -23,56 +23,5 @@ public class PreferenceConstants {
   
    /** The Constant SCREEN_CONTROLLER. */
   public static final String SCREEN_CONTROLLER = "ScreenControllerFile";
- 
-  /** The Constant PTM_RATIO. */
-  public static final String PTM_RATIO = "PTM_RATIO";
-
-  /** The Constant GRAVITY_X. */
-  public static final String GRAVITY_X = "GRAVITY_X";
-
-  /** The Constant GRAVITY_Y. */
-  public static final String GRAVITY_Y = "GRAVITY_Y";
-
-  /** The Constant TIMESTEP. */
-  public static final String TIMESTEP = "TIMESTEP";
-
-  /** The Constant VELOCITY_ITERATIONS. */
-  public static final String VELOCITY_ITERATIONS = "VELOCITY_ITERATIONS";
-
-  /** The Constant POSITION_ITERATIONS. */
-  public static final String POSITION_ITERATIONS = "POSITION_ITERATIONS";
-
-  /** The Constant DRAW_BODIES. */
-  public static final String DRAW_BODIES = "BODIES_BIT";
-
-  /** The Constant DRAW_JOINT. */
-  public static final String DRAW_JOINT = "JOINT_BIT ";
-
-  /** The Constant DRAW_AABB. */
-  public static final String DRAW_AABB = "AABB_BIT ";
-
-  /** The Constant DRAW_INACTIVE_BODIES. */
-  public static final String DRAW_INACTIVE_BODIES = "INACTIVE_BODIES_BIT ";
-
-  /** The Constant DRAW_DEBUG_DATA. */
-  public static final String DRAW_DEBUG_DATA = "DRAW_DEBUG_DATA_BIT ";
-
-  /** The Constant DRAW_ENTITIES. */
-  public static final String DRAW_ENTITIES = "DRAW_ENTITIES ";
-
-  /** The Constant INSTALL_MOUSE_JOINT. */
-  public static final String INSTALL_MOUSE_JOINT = "INSTALL_MOUSE_JOINT ";
-
-  /** The Constant CARD_WIDTH. */
-  public static final String CARD_WIDTH = "CARD_WIDTH ";
-
-  /** The Constant CARD_HEIGHT. */
-  public static final String CARD_HEIGHT = "CARD_HEIGHT ";
-
-  /** The Constant CARD_NO_X. */
-  public static final String CARD_NO_X = "CARD_NO_X ";
-
-  /** The Constant CARD_NO_Y. */
-  public static final String CARD_NO_Y = "CARD_NO_Y ";
 
 }
