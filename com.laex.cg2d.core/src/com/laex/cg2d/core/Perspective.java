f31c9c74a96b497cfd6f916b7722fc43a252abfb
Hemanta Sapkota <laex.pearl@gmail.com>
2013-06-02 21:49:17 +1000

    Add LayerOutlineViewPart

--- com.laex.cg2d.core/src/com/laex/cg2d/core/Perspective.java
+++ com.laex.cg2d.core/src/com/laex/cg2d/core/Perspective.java
@@ -14,6 +14,7 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import com.laex.cg2d.screeneditor.views.LayerOutlineViewPart;
 import com.laex.cg2d.screeneditor.views.LayersViewPart;
 import com.laex.cg2d.screeneditor.views.TexturesViewPart;
 
