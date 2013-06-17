fb05927883f7b5f8c81a477fb1b7ff6f61a30815
Hemanta Sapkota <laex.pearl@gmail.com>
2013-06-17 23:51:08 +1000

    validate if redo should be executed

--- com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/handlers/RedoHandler.java
+++ com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/handlers/RedoHandler.java
@@ -3,13 +3,24 @@ package com.laex.cg2d.screeneditor.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.ui.actions.RedoAction;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class RedoHandler extends AbstractHandler {
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
+    IEditorPart ep = HandlerUtil.getActiveEditor(event);
+    GraphicalViewer gv = (GraphicalViewer) ep.getAdapter(GraphicalViewer.class);
+    
+    
+    if (!gv.getEditDomain().getCommandStack().canRedo()) {
+      return null;
+    }
+    
+    
     RedoAction redo = new RedoAction(HandlerUtil.getActiveEditor(event));
     redo.run();
     return null;
