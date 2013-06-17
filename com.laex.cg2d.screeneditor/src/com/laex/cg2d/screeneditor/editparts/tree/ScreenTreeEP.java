8ec9f520fa91e4a656d10bc1d6d13071d5de6add
Hemanta Sapkota <laex.pearl@gmail.com>
2013-06-17 23:50:02 +1000

    addChildVisual() customizes layer tree edit part by appropriately placing shape edit parts in to layer edit parts

--- com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/editparts/tree/ScreenTreeEP.java
+++ com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/editparts/tree/ScreenTreeEP.java
@@ -7,10 +7,16 @@ import java.util.List;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.RootEditPart;
+import org.eclipse.gef.TreeEditPart;
 import org.eclipse.gef.editparts.AbstractTreeEditPart;
 import org.eclipse.gef.editpolicies.RootComponentEditPolicy;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.swt.widgets.Widget;
 
+import com.laex.cg2d.model.model.Layer;
 import com.laex.cg2d.model.model.ModelElement;
+import com.laex.cg2d.model.model.Shape;
 import com.laex.cg2d.model.model.ShapesDiagram;
 
 public class ScreenTreeEP extends AbstractTreeEditPart implements PropertyChangeListener {
@@ -42,19 +48,42 @@ public class ScreenTreeEP extends AbstractTreeEditPart implements PropertyChange
   @Override
   public void propertyChange(PropertyChangeEvent evt) {
     String prop = evt.getPropertyName();
-    if (ShapesDiagram.CHILD_ADDED_PROP.equals(prop)) {
+    if (ShapesDiagram.CHILD_ADDED_PROP.equals(prop) || ShapesDiagram.LAYER_ADDED.equals(prop)) {
       // add a child to this edit part
-      // causes an additional entry to appear in the tree of the outline
-      // view
       addChild(createChild(evt.getNewValue()), -1);
-    } else if (ShapesDiagram.CHILD_REMOVED_PROP.equals(prop)) {
+    } else if (ShapesDiagram.CHILD_REMOVED_PROP.equals(prop) || ShapesDiagram.LAYER_REMOVED.equals(prop)) {
       // remove a child from this edit part
-      // causes the corresponding edit part to disappear from the tree in
-      // the outline view
-      removeChild(getEditPartForChild(evt.getNewValue()));
+      EditPart ep = getEditPartForChild(evt.getNewValue());
+      removeChild(ep);
     } else {
       refreshVisuals();
     }
+    
+    refreshVisuals();
+  }
+
+  @Override
+  protected void addChildVisual(EditPart childEditPart, int index) {
+    //Oveeride from abstract class.
+    Widget widget = getWidget();
+    TreeItem item;
+
+    if (widget instanceof Tree)
+      item = new TreeItem((Tree) widget, 0, index);
+    else
+      item = new TreeItem((TreeItem) widget, 0, index);
+
+    //If it is shape model, then we put it inside its layer
+    if (childEditPart.getModel() instanceof Shape) {
+      Shape shpM = (Shape) childEditPart.getModel();
+      Layer layer = shpM.getParentLayer();
+      LayerTreeEP ltep = (LayerTreeEP) getEditPartForChild(layer);
+
+      int trueIndex = ltep.getChildren().size() - 1;
+      item = new TreeItem((TreeItem) ltep.getWidget(), 0, (trueIndex <= 0) ? 0 : trueIndex);
+    }
+
+    ((TreeEditPart) childEditPart).setWidget(item);
   }
 
   @Override
