acfb68c3a9001d7852dd9007af678ae5070f02e7
Hemanta Sapkota <laex.pearl@gmail.com>
2013-06-17 23:56:06 +1000

    minor refactor.

--- com.laex.cg2d.shared/src/com/laex/cg2d/model/model/Shape.java
+++ com.laex.cg2d.shared/src/com/laex/cg2d/model/model/Shape.java
@@ -24,9 +24,9 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.laex.cg2d.model.activator.Activator;
-import com.laex.cg2d.model.model.descs.BodyDefPropertySource;
-import com.laex.cg2d.model.model.descs.FixtureDefPropertySource;
-import com.laex.cg2d.model.model.descs.RectanglePropertySource;
+import com.laex.cg2d.model.descs.BodyDefPropertySource;
+import com.laex.cg2d.model.descs.FixtureDefPropertySource;
+import com.laex.cg2d.model.descs.RectanglePropertySource;
 
 /**
  * The Class Shape.
