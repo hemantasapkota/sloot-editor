ca9ddaa230c96ab52647b686e1f9106e3dab31b1
Hemanta Sapkota <laex.pearl@gmail.com>
2013-07-08 01:02:02 +1000

    Minor refactor.

--- com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/editparts/tree/LayerTreeEP.java
+++ com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/editparts/tree/LayerTreeEP.java
@@ -26,17 +26,19 @@ import com.laex.cg2d.model.model.ModelElement;
  */
 public class LayerTreeEP extends AbstractTreeEditPart implements PropertyChangeListener {
 
-
   /**
    * Instantiates a new layer tree ep.
-   *
-   * @param layer the layer
+   * 
+   * @param layer
+   *          the layer
    */
   public LayerTreeEP(Layer layer) {
     super(layer);
   }
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.eclipse.gef.editparts.AbstractEditPart#activate()
    */
   public void activate() {
@@ -45,8 +47,10 @@ public class LayerTreeEP extends AbstractTreeEditPart implements PropertyChangeL
       ((ModelElement) getModel()).addPropertyChangeListener(this);
     }
   }
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.eclipse.gef.editparts.AbstractEditPart#deactivate()
    */
   public void deactivate() {
@@ -54,26 +58,29 @@ public class LayerTreeEP extends AbstractTreeEditPart implements PropertyChangeL
       super.deactivate();
       ((ModelElement) getModel()).removePropertyChangeListener(this);
     }
-  }  
+  }
 
-  /* (non-Javadoc)
+   /* (non-Javadoc)
    * @see org.eclipse.gef.editparts.AbstractEditPart#getModelChildren()
    */
   @Override
-  protected List getModelChildren() {
+  protected List<?> getModelChildren() {
     return getCastedModel().getChildren();
   }
 
+
   /**
    * Gets the casted model.
-   *
+   * 
    * @return the casted model
    */
   private Layer getCastedModel() {
     return (Layer) getModel();
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.eclipse.gef.editparts.AbstractTreeEditPart#getImage()
    */
   @Override
@@ -81,22 +88,30 @@ public class LayerTreeEP extends AbstractTreeEditPart implements PropertyChangeL
     return SharedImages.LAYER.createImage();
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.eclipse.gef.editparts.AbstractTreeEditPart#getText()
    */
   @Override
   protected String getText() {
     return getCastedModel().getName();
   }
-
-  /* (non-Javadoc)
-   * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+  
+  
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent
+   * )
    */
   @Override
   public void propertyChange(PropertyChangeEvent evt) {
     if (evt.getPropertyName().equals(Layer.LAYER_NAME_CHANGED)) {
       refreshVisuals();
-    }
+    } 
   }
 
 }
