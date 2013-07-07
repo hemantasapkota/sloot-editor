22153dae21c20e39254d75995e936a590b5d2c3e
Hemanta Sapkota <laex.pearl@gmail.com>
2013-07-08 01:06:02 +1000

    External animation preview

---
+++ com.laex.cg2d.entityeditor/src/com/laex/cg2d/entityeditor/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package com.laex.cg2d.entityeditor.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String ANIMATION_PREVIEW = "animationPreview";
+	
+}
