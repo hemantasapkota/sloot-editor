43a43ae375e6a2e1d2a63abe86545d636030e879
Hemanta Sapkota <laex.pearl@gmail.com>
2013-07-18 17:07:53 +1000

    Use generic box image for displaying items in the list

--- com.laex.cg2d.entityeditor/src/com/laex/cg2d/entityeditor/pages/AnimationTableLabelProvider.java
+++ com.laex.cg2d.entityeditor/src/com/laex/cg2d/entityeditor/pages/AnimationTableLabelProvider.java
@@ -1,5 +1,4 @@
-/*
- * Copyright (c) 2012, 2013 Hemanta Sapkota.
+/* * Copyright (c) 2012, 2013 Hemanta Sapkota.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +13,8 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
+import com.laex.cg2d.model.SharedImages;
+
 /**
  * The Class AnimationTableLabelProvider.
  */
@@ -27,7 +28,7 @@ class AnimationTableLabelProvider extends LabelProvider implements ITableLabelPr
    * Object, int)
    */
   public Image getColumnImage(Object element, int columnIndex) {
-    return ((AnimationListViewItem) element).getFirstFrame();
+    return SharedImages.BOX.createImage(); 
   }
 
   /*
