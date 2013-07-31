07df0505480fa278b1b2233fe6a4a22484a23986
Hemanta Sapkota <laex.pearl@gmail.com>
2013-08-01 07:43:57 +1000

    refactorings

--- com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/model/CGScreenModelAdapter.java
+++ com.laex.cg2d.screeneditor/src/com/laex/cg2d/screeneditor/model/CGScreenModelAdapter.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
