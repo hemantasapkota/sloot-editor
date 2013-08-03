dfbb7281838d30b6f92bc79df4f7a0baa40b566f
Hemanta Sapkota <laex.pearl@gmail.com>
2013-08-03 16:07:41 +1000

    add image flipping methods from swt snippets

--- com.laex.cg2d.shared/src/com/laex/cg2d/model/resources/ResourceManager.java
+++ com.laex.cg2d.shared/src/com/laex/cg2d/model/resources/ResourceManager.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.CompositeImageDescriptor;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
@@ -66,6 +67,69 @@ public class ResourceManager extends SWTResourceManager {
    */
   public static ImageDescriptor getImageDescriptor(Class<?> clazz, String path) {
     return ImageDescriptor.createFromFile(clazz, path);
+  }
+
+  public static ImageData rotate(ImageData srcData, int direction) {
+    int bytesPerPixel = srcData.bytesPerLine / srcData.width;
+    int destBytesPerLine = (direction == SWT.DOWN) ? srcData.width * bytesPerPixel : srcData.height * bytesPerPixel;
+    byte[] newData = new byte[(direction == SWT.DOWN) ? srcData.height * destBytesPerLine : srcData.width
+        * destBytesPerLine];
+    int width = 0, height = 0;
+    for (int srcY = 0; srcY < srcData.height; srcY++) {
+      for (int srcX = 0; srcX < srcData.width; srcX++) {
+        int destX = 0, destY = 0, destIndex = 0, srcIndex = 0;
+        switch (direction) {
+        case SWT.LEFT: // left 90 degrees
+          destX = srcY;
+          destY = srcData.width - srcX - 1;
+          width = srcData.height;
+          height = srcData.width;
+          break;
+        case SWT.RIGHT: // right 90 degrees
+          destX = srcData.height - srcY - 1;
+          destY = srcX;
+          width = srcData.height;
+          height = srcData.width;
+          break;
+        case SWT.DOWN: // 180 degrees
+          destX = srcData.width - srcX - 1;
+          destY = srcData.height - srcY - 1;
+          width = srcData.width;
+          height = srcData.height;
+          break;
+        }
+        destIndex = (destY * destBytesPerLine) + (destX * bytesPerPixel);
+        srcIndex = (srcY * srcData.bytesPerLine) + (srcX * bytesPerPixel);
+        System.arraycopy(srcData.data, srcIndex, newData, destIndex, bytesPerPixel);
+      }
+    }
+    // destBytesPerLine is used as scanlinePad to ensure that no padding is
+    // required
+    return new ImageData(width, height, srcData.depth, srcData.palette, srcData.scanlinePad, newData);
+  }
+
+  public static ImageData flip(ImageData srcData, boolean vertical) {
+    int bytesPerPixel = srcData.bytesPerLine / srcData.width;
+    int destBytesPerLine = srcData.width * bytesPerPixel;
+    byte[] newData = new byte[srcData.data.length];
+    for (int srcY = 0; srcY < srcData.height; srcY++) {
+      for (int srcX = 0; srcX < srcData.width; srcX++) {
+        int destX = 0, destY = 0, destIndex = 0, srcIndex = 0;
+        if (vertical) {
+          destX = srcX;
+          destY = srcData.height - srcY - 1;
+        } else {
+          destX = srcData.width - srcX - 1;
+          destY = srcY;
+        }
+        destIndex = (destY * destBytesPerLine) + (destX * bytesPerPixel);
+        srcIndex = (srcY * srcData.bytesPerLine) + (srcX * bytesPerPixel);
+        System.arraycopy(srcData.data, srcIndex, newData, destIndex, bytesPerPixel);
+      }
+    }
+    // destBytesPerLine is used as scanlinePad to ensure that no padding is
+    // required
+    return new ImageData(srcData.width, srcData.height, srcData.depth, srcData.palette, srcData.scanlinePad, newData);
   }
 
   /**
