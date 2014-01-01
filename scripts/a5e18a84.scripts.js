7388898b15152d92437170c1f6ce3ab2a401042d
Hemanta Sapkota <laex.pearl@gmail.com>
2014-01-01 22:12:45 +1100

    Updates

---
+++ scripts/a5e18a84.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("ghPagesSrcApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("ghPagesSrcApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
