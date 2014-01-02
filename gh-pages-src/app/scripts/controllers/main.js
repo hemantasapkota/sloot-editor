94865d258523231ef253031909a8f44470a142a2
Hemanta Sapkota <laex.pearl@gmail.com>
2014-01-02 12:05:54 +1100

    Update pages

--- gh-pages-src/app/scripts/controllers/main.js
+++ gh-pages-src/app/scripts/controllers/main.js
@@ -7,17 +7,15 @@ angular.module('ghPagesSrcApp')
     var slides = $scope.slides = [];
 
     $scope.addSlide = function(img, desc) {
-
       slides.push({
         image: img,
         text: desc
       });
-
     };
 
-    $scope.addSlide('https://raw.github.com/hemantasapkota/sloot-editor/master/screenshots/EdLuaScript.png', 'Sloot Editor');
-    $scope.addSlide('', 'Sloot Editor');
-    $scope.addSlide('', 'Sloot Editor');
-    $scope.addSlide('', 'Sloot Editor');
+    $scope.addSlide('https://raw.github.com/hemantasapkota/sloot-editor/master/screenshots/s2/SlootEditorEntity.png', 'Sloot Editor');
+    $scope.addSlide('https://raw.github.com/hemantasapkota/sloot-editor/master/screenshots/s2/SlootEditorRender.png', 'Render View');
+    $scope.addSlide('https://raw.github.com/hemantasapkota/sloot-editor/master/screenshots/s2/SlootEditorEntity.png', 'Entity Editor');
+    $scope.addSlide('https://raw.github.com/hemantasapkota/sloot-editor/master/screenshots/s2/SlootEditorScripting.png', 'Scripting');
 
   });
